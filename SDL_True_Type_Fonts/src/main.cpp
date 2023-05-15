2013c191aa8e748f19d8a78be213765bd039bf79
Emiliano Iacopini <iacoviola.tb@gmail.com>
2023-05-15 17:26:36 +0200

    updated makefiles and added project: SDL_True_Type_Fonts

---
+++ SDL_True_Type_Fonts/src/main.cpp
@@ -0,0 +1,133 @@
+//
+//  main.cpp
+//  SDL_Keying
+//
+//  Created by Emiliano Iacopini on 3/12/23.
+//
+
+#include <stdio.h>
+
+#include "LTexture.hpp"
+#include "Globals.hpp"
+#include "Constants.hpp"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+TTF_Font* font = NULL;
+
+LTexture textTexture;
+
+bool init(){
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("SDL could not initialize! SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    
+    if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")){
+        printf("Warning: linear texture filtering not enabled!");
+    }
+    
+    window = SDL_CreateWindow("SLD True type Fonts", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    
+    if(window == NULL){
+        printf("Window could not be created! SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    if(renderer == NULL){
+        printf("Renderer could not be created! SDL error: %s\n", SDL_GetError());
+        return false;
+    }
+    
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    
+    int imgflags = IMG_INIT_PNG;
+    if(!(IMG_Init(imgflags) & imgflags)){
+        printf("SDL_image could not initialize! SDL_image error: %s\n", IMG_GetError());
+        return false;
+    }
+
+    //Initialize SDL_ttf
+    if(TTF_Init() == -1){
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return false;
+    }
+    
+    return true;
+}
+
+bool loadMedia(){
+    
+    font = TTF_OpenFont("../res/lazy.ttf", 40);
+    if(font == NULL){
+        printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+        return false;
+    }
+
+    SDL_Color textColor = {0, 0, 0};
+
+    if(!textTexture.loadFromRenderedText("Hello Bitch!", textColor)){
+        printf("Failed to load front texture image!\n");
+        return false;
+    }
+    
+    return true;
+}
+
+void close(){
+    textTexture.free();
+
+    TTF_CloseFont(font);
+    font = NULL;
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    
+    renderer = NULL;
+    window = NULL;
+    
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, const char * argv[]) {
+    if(!init()){
+        printf("Failed to initialize!\n");
+        return 0;
+    }
+    if(!loadMedia()){
+        printf("Failed to load media!\n");
+        return 0;
+    }
+    
+    //Hack to get window to stay up
+    SDL_Event e;
+    bool quit = false;
+    while( quit == false ){
+        while( SDL_PollEvent( &e ) ){
+            if( e.type == SDL_QUIT){
+                quit = true;
+            } else if(e.type == SDL_KEYDOWN){
+                switch (e.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                }
+            }
+        }
+        
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+        
+        textTexture.render((SCREEN_WIDTH - textTexture.getWidth()) / 2, (SCREEN_HEIGHT - textTexture.getHeight()) / 2);
+                
+        SDL_RenderPresent(renderer);
+    }
+
+    close();
+    return 0;
+}
